843554bb5471bf03a0f5845ff1268db06c9a61b8
Spagetik <lazo12kirill@gmail.com>
2022-10-20 23:33:09 +0200

    Add tests

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import json
 from subprocess import CompletedProcess, run, PIPE
+from typing import Dict, List, Tuple
 
 with open('tests.json') as f:
     data = json.load(f)
 
 
-def run_test(app_name: str, args: str, stdin: str):
-    p = run(app_name + args, input=stdin, encoding='ascii', stdout=PIPE, stderr=PIPE)
+def run_test(app_name: str, args: List[str], stdin: str):
+    p = run([app_name] + args, input=stdin, encoding='ascii', stdout=PIPE, stderr=PIPE)
     print(p.stdout)
     print(p.stderr)
 
@@ -19,4 +20,4 @@ if __name__ == '__main__':
         test = tests[test_i]
         args = test['args']
         s = " ".join(args)
-        run_test('./t9search ', s, stdin[0])
+        run_test('./t9search', args, stdin[0])
