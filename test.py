fc9f1abc90daad8c6fc24ce3cece59e04670eb03
Spagetik <lazo12kirill@gmail.com>
2022-10-20 23:39:08 +0200

    Add tests

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ with open('tests.json') as f:
 
 def run_test(app_name: str, args: List[str], stdin: str, stdout: str):
     p = run([app_name] + args, input=stdin, encoding='ascii', stdout=PIPE, stderr=PIPE)
-    print(p.stdout[:-1])
-    print(stdout)
-    if p.stdout == stdout:
+    # print(p.stdout[:-1])
+    # print(stdout)
+    if p.stdout[:-1] == stdout:
         print('PASSED')
 
 
